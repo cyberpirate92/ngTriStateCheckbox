44e242917aa8bbd1b8cb0f0f9f4065255136b86d
cyberpirate92 <bsravi.tez@gmail.com>
2017-10-20 18:28:31 +0530

    Improved sample code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,11 @@ import { Component } from '@angular/core';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-    title = 'app';
-    checkboxState = false;
-    public onCheckboxStateChange(value: boolean): void {
-        console.log('Checkbox State: ' + value);
-        this.checkboxState = value;
-    }
+
+    public checkboxState = false;
+
     public onClick(): void {
         this.checkboxState = null;
     }
